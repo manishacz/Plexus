0c5ba125f5eb035098ce0d7d467378698c783c51
manishacz <u22ee063@eed.svnit.ac.in>
2025-12-14 12:00:23 +0530

    bug fix

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -50,7 +50,7 @@ const Login = () => {
     return (
         <div className="login-container">
             <div className="login-logo-header">
-               <span className="logo-text">PlexusChat</span>
+               <span className="logo-text">Plexus</span>
             </div>
             
             <div className="login-content">
